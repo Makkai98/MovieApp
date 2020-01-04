ceec2dad55ff42ff25845ebc98231be3be96e1e8
Makkai98 <makkaimatyas00@gmail.com>
2020-01-04 16:52:43 +0200

    Final version

--- app/src/main/java/com/example/movieapp/Adapter/MoviesAdapter.java
+++ app/src/main/java/com/example/movieapp/Adapter/MoviesAdapter.java
@@ -4,10 +4,12 @@ package com.example.movieapp.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.movieapp.Model.Movie;
 import com.example.movieapp.R;
 
@@ -15,6 +17,7 @@ import java.util.List;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
 
+   // private String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w500";
     private List<Movie> movies;
 
     public MoviesAdapter() {
@@ -45,6 +48,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         TextView title;
         TextView rating;
         TextView genres;
+        ImageView poster;
 
         public MovieViewHolder(View itemView) {
             super(itemView);
@@ -52,13 +56,15 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
             title = itemView.findViewById(R.id.item_movie_title);
             rating = itemView.findViewById(R.id.item_movie_rating);
             genres = itemView.findViewById(R.id.item_movie_genre);
+           // poster = itemView.findViewById(R.id.item_movie_poster);
         }
 
         public void bind(Movie movie) {
             releaseDate.setText(movie.getReleaseDate().split("-")[0]);
             title.setText(movie.getTitle());
-           // rating.setText(String.valueOf(movie.getVoteAverage()));
+            rating.setText(String.valueOf(movie.getRating()));
             genres.setText("");
+           // Glide.with(itemView).load(IMAGE_BASE_URL + movie.getPosterPath()).into(poster);
         }
     }
 }
